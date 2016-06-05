e04532bc647fcfe4efac1cce1fa37d901a5311de
Stanley Yeh <ihyeh@mosi.com.tw>
2016-06-05 23:10:26 +0800

    Added support for question 2 and 3

--- app.js
+++ app.js
@@ -78,9 +78,11 @@ dialog.matches('\\bstatus\\b', [
     }
 ]);
 
-dialog.matches('\\bperformance\\b', askStationPerformance);
+dialog.matches('\\bperformance\\b', [askStationPerformance, detailFollowup('performance')]);
 
-dialog.matches('\\bschedule\\b', builder.DialogAction.send('You are asking for on-time schedule.'));
+dialog.matches('\\bschedule\\b', [askSchedule, detailFollowup('schedule')]);
+
+//dialog.matches('\\bovertime\\b', [askOvertime, detailFollowup('overtime')]);
 
 slackBot.listenForMentions();
 
@@ -91,23 +93,149 @@ bot.startRTM( function(err, bot, payload) {
 });
 
 function askStationPerformance(session, args) {
-    console.log(args)
+    var param = args.matches['input'].trim().split(' ');
+    session.dialogData.stationName = param[0];
+    session.dialogData.period = param[2];
+    
     request({
         url: 'http://demo.mosi.com.tw:3003/bot/question2',
-        qs: { station: 'TestPanel6', time: 'today' },
+        qs: { station: param[0], time: param[2] },
         method: 'GET'
     }, function(error, response, body) {
         if(error) {
             console.log(error);
         } else {
-            console.log(body);
             if(response.statusCode !== 200 || body.slice(0,11) == 'serverError') {
                 session.send(prompts.generalError);
             } else {
-                //var result = JSON.parse(body);
-                //console.log(result);
-                session.send('ok');
+                var result = JSON.parse(body);
+                builder.Prompts.text(session, sprintf(prompts.answerPerformance, result));
             }
         }
     });
+}
+
+function askSchedule(session, args) {
+    request({
+        url: 'http://demo.mosi.com.tw:3003/bot/question3',
+        method: 'GET'
+    }, function(error, response, body) {
+        if(error) {
+            console.log(error);
+        } else {
+            if(response.statusCode !== 200 || body.slice(0,11) == 'serverError') {
+                session.send(prompts.generalError);
+            } else {
+                var result = JSON.parse(body);
+                var reply = sprintf(prompts.answerSchedule, result);
+                
+                if(result.delayMaster && result.delayMaster.length > 0) {
+                    var appendTxt = 'Below are the delayed jobs:\n';
+                    for(var i in result.delayMaster) {
+                        appendTxt += "* " + result.delayMaster[i] + "\n";
+                    }
+                    reply += appendTxt;
+                }
+                builder.Prompts.text(session, reply);
+            }
+        }
+    });
+}
+
+/*
+function askOvertime(session, args) {
+    var param = args.matches['input'].trim().split(' ');
+    if(param.length > 3) {
+        session.dialogData.period = param[2] + param[3];
+    } else {
+        session.dialogData.period = param[2];
+    }
+    
+    request({
+        url: 'http://demo.mosi.com.tw:3003/bot/question4',
+        qs: { time: session.dialogData.period },
+        method: 'GET'
+    }, function(error, response, body) {
+        if(error) {
+            console.log(error);
+        } else {
+            if(response.statusCode !== 200 || body.slice(0,11) == 'serverError') {
+                session.send(prompts.generalError);
+            } else {
+                var result = JSON.parse(body);
+                console.log(result);
+                var reply = sprintf(prompts.answerOvertime, result);
+                
+                if(result.list && result.list.length > 0) {
+                    var appendTxt = '\nOvertime report:\n';
+                    for(var i in result.list) {
+                        var key = Object.keys(result.list[i])[0];
+                        appendTxt += "* " + key + ": " + result.list[i][key] + "min.\n";
+                    }
+                    reply += appendTxt;
+                }
+                builder.Prompts.text(session, reply);
+            }
+        }
+    });
+}
+*/
+
+function detailFollowup(field) {
+    return function(session, results) {
+        if(results.response && results.response.slice(0,6) == 'losses') {
+            if(field == 'performance') {
+                request({
+                    url: 'http://demo.mosi.com.tw:3003/bot/reasonList',
+                    qs: { type: 'station', time: session.dialogData.period, num: 1, name: session.dialogData.stationName },
+                    method: 'GET'
+                }, function(error, response, body) {
+                    if(error) {
+                        console.log(error);
+                    } else {
+                        if(response.statusCode !== 200 || body.slice(0,11) == 'serverError') {
+                            session.send(prompts.generalError);
+                        } else {
+                            var result = JSON.parse(body);
+                            var reply = 'Here are the top 3 reasons for efficiency losses:\n';
+                            for(var i in result.reasonList) {
+                                reply += "* " + result.reasonList[i] + "\n";
+                            }
+                            session.send(reply);
+                        }
+                    }
+                });
+            }
+            else if(field == 'schedule') {
+                var param = results.response.trim().split(' ');
+                var jobid = param[1]
+                
+                request({
+                    url: 'http://demo.mosi.com.tw:3003/bot/reasonList',
+                    qs: { type: 'job', num: 1, name: jobid },
+                    method: 'GET'
+                }, function(error, response, body) {
+                    if(error) {
+                        console.log(error);
+                    } else {
+                        if(response.statusCode !== 200 || body.slice(0,11) == 'serverError') {
+                            session.send(prompts.generalError);
+                        } else {
+                            var result = JSON.parse(body);
+                            var reply = 'Here are the top 3 reasons for efficiency losses:\n';
+                            for(var i in result.reasonList) {
+                                reply += "* " + result.reasonList[i] + "\n";
+                            }
+                            session.send(reply);
+                        }
+                    }
+                });
+            }
+            else {
+                session.send(prompts.abort);
+            }
+        } else {
+            session.send(prompts.abort);
+        }
+    }
 }
--- prompts.js
+++ prompts.js
@@ -1,8 +1,9 @@
 module.exports = {
     helpMsg: "I can help you with briefings to find out how our production is doing. You can ask me things like:\n\n" +
     "* what's the status?\n" +
-    "* show me the performance of Line3\n" +
+    "* Line3 performance today\n" +
     "* are we on schedule?\n" +
+    "* overtime needed yesterday" +
     "\nSay 'nevermind' during our conversation to let me back to work!",
     greeting: "Hi %s, how may I help you?",
     abort: "Sure, glad to help.",
@@ -10,6 +11,8 @@ module.exports = {
     answerStatus: "%(production)f%% of the machines are working on assigned jobs, and %(normal)f%% of those machines are running normally.\n" +
     "Say _detail_ for a list of problematic machines.",
     answerStatusBreak: "We are having a break from %(expDown)s.",
-    answerPerformance: "OEE of the requested period was %(oee)f%% (Availability: %(availability)f%%; Speed: %(speed)f%%; Quality: %(quality)f%%).",
-    answerSchedule: "",
+    answerPerformance: "OEE of the requested period was %(oee)f%% (Availability: %(availability)f%%; Speed: %(speed)f%%; Quality: %(quality)f%%).\n" +
+    "Say _losses_ to show top 3 losses affecting performance during the period.",
+    answerSchedule: "%(onTimeRate)f%% of the jobs currently in production are on track to finish in time.",
+    answerOvertime: "Total overtime of the requested period were %(total)d minutes."
 }
